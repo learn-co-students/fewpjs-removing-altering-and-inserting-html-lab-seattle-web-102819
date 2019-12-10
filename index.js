4a26bb686232a57f86653faf32e3d884079146f7
bahayg <bahayg@gmail.com>
2019-12-10 15:16:31 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+
+main.remove()
+
+let newHeader = document.createElement('h1');
+newHeader.setAttribute("id", "victory");
+newHeader.innerHTML = 'Bahay is the champion'
+document.appendChild(newHeader);
