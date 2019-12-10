03db616cd66c028ba4becd216789833adf840eec
bahayg <bahayg@gmail.com>
2019-12-10 15:19:47 -0800

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 // Write your code here!
 
-main.remove()
+document.querySelector("main#main").remove();
+// main.remove()
 
-let newHeader = document.createElement('h1');
+const newHeader = document.createElement('h1');
 newHeader.setAttribute("id", "victory");
-newHeader.innerHTML = 'Bahay is the champion'
-document.appendChild(newHeader);
+// newHeader.id =  "victory";
+newHeader.innerHTML = 'Bahay is the champion';
+// document.appendChild(newHeader);
